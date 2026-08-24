338f5856089f352d03b5bbf88923f6938bbb12b8
brunodavi <brunodaviandrade2000+termux@gmail.com>
2026-08-23 21:43:37 -0300

    test: RED do round-robin por modelo com thread-safety

---
+++ src/utils/round_robin.py
@@ -0,0 +1,7 @@
+class RoundRobin:
+    def __init__(self, model_keys):
+        raise NotImplementedError("round-robin por modelo")
+
+
+def next(model):
+    raise NotImplementedError("round-robin por modelo")
